893e6a703b6bb8d863e3ee5aadaac5c98dc4dc3b
iigmir <roc120j@gmail.com>
2017-07-31 02:56:39 +0800

    HTML modulise

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,14 @@ var browserSync = require('browser-sync').create();
 var gulpSass    = require('gulp-sass');
 var uglify      = require('gulp-uglify');
 var pump        = require('pump');
+var extender    = require('gulp-html-extend');
 
 gulp.task('browser-sync', function()
 {
-    browserSync.init({
-        server: {
-            baseDir: "./docs"
-        }
+    browserSync.init
+    ({
+        server: { baseDir: "./docs" }
     });
-});
-
-gulp.task('watch', function ()
-{
-    gulp.watch("src/scss/*.scss", ['sass']);
-    gulp.watch("src/js/*.js", ['jscompress']);
-    gulp.watch("docs/*/*").on('change', browserSync.reload);
-    gulp.watch("docs/*").on('change', browserSync.reload);
 });
 
 gulp.task('jscompress', function(cb)
@@ -41,5 +33,20 @@ gulp.task('sass', function()
         .pipe(browserSync.stream());
 });
 
+gulp.task('html', function()
+{
+    gulp.src('src/html/index.html')
+        .pipe(extender({annotations:false,verbose:true}))
+        .pipe(gulp.dest('./docs'));
+});
+
+gulp.task('watch', function ()
+{
+    gulp.watch('src/html/*.html', ['html']);
+    gulp.watch("src/scss/*.scss", ['sass']);
+    gulp.watch("src/js/*.js", ['jscompress']);
+    gulp.watch("docs/*/*").on('change', browserSync.reload);
+    gulp.watch("docs/*").on('change', browserSync.reload);
+});
 
 gulp.task('default',['browser-sync','watch']);
